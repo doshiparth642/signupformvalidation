c3ee9708de0d7ef215917f9a26c22b21af036fb3
doshiparth642 <91174956+doshiparth642@users.noreply.github.com>
2021-09-24 08:44:58 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './style.css'
+import Form from './Components/Form';
+
+
+function App() {
+			return(
+		
+		<div className='App'>
+
+				  <Form />
+		</div>
+      
+
+	)
+	}
+export default App;
+
+
